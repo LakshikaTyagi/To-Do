aeb0cfde2062cfd879517e0a82d3e7c69b67d3d8
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-01-31 13:03:25 +0530

    sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Sidebar() {
   return (
-    <div className='flex flex-col justify-between w-73'>
+    <div className='flex flex-col justify-between box-border h-screen m-0 p-0 overflow-hidden w-73'>
 
       <div>
 
@@ -58,9 +58,10 @@ function Sidebar() {
 
       </div>
 
-      <div className='flex justify-center items-center'>
-        <Link className=''>
-        </Link>
+      <div >
+        <a href="https://outlook.live.com/mail/">
+        <svg class="fluentIcon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15.5 4A2.5 2.5 0 0118 6.5v8a2.5 2.5 0 01-2.5 2.5h-11A2.5 2.5 0 012 14.5v-8A2.5 2.5 0 014.5 4h11zM17 7.96l-6.75 3.97a.5.5 0 01-.42.04l-.08-.04L3 7.96v6.54c0 .83.67 1.5 1.5 1.5h11c.83 0 1.5-.67 1.5-1.5V7.96zM15.5 5h-11C3.67 5 3 5.67 3 6.5v.3l7 4.12 7-4.12v-.3c0-.83-.67-1.5-1.5-1.5z" fill="currentColor"></path></svg>
+        </a>
       </div>
     </div>
   )
