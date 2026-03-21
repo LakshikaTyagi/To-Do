47963c5a0f5df12ade5fc74bee39eb4417e95a08
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-03-21 18:43:43 +0530

    added task

--- src/pages/MyDay/MyDay.jsx
+++ src/pages/MyDay/MyDay.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import { getDate } from '../../../utils/utils';
 import { Star } from 'lucide-react';
+import { Circle } from 'lucide-react';
 
 function MyDay(props) {
   const todayDate = getDate();
   const [task, setTask] = useState({
-    id:"",
+    id: "",
     title: "",
     date: "",
-    isImportant:false,
+    isImportant: false,
+    isCompleted : false,
   });
   const [allTasks, setAllTasks] = useState([]);
 
@@ -20,33 +22,64 @@ function MyDay(props) {
     })
   }
 
-  const handleAddTask=()=>{
+  const handleAddTask = () => {
     setAllTasks([
       ...allTasks,
       task
     ])
 
     setTask({
-    title: "",
-    date: "",
-    isImportant:false,
-    id:"",
-  })
+      title: "",
+      date: "",
+      isImportant: false,
+      isCompleted : false,
+      id: "",
+    })
   }
 
-  const handleMarkImportant =(index)=>{
-    setAllTasks((tasks)=>{
-      return tasks.map((task,i)=>{
-       if(i===index){
-        return{
-          ...task,
-          isImportant: !task?.isImportant
+  // const handleCompleteTask = () => {
+  //   setAllTasks([
+  //     ...allTasks,
+  //     task
+  //   ])
+
+  //   setTask({
+  //     title : "",
+  //     date: "",
+  //     isImportant: false,
+  //     isCompleted : false,
+  //     id : "",
+  //   })
+  // }
+
+  const handleMarkImportant = (index) => {
+    setAllTasks((tasks) => {
+      return tasks.map((task, i) => {
+        if (i === index) {
+          return {
+            ...task,
+            isImportant: !task?.isImportant
+          }
         }
-       }
-       else return task
+        else return task
       })
     })
   }
+
+  const handleMarkComplete = (index) => {
+    setAllTasks((tasks) => {
+      return tasks.map((task, i) => {
+        if(i === index){
+          return {
+            ...task,
+            isCompleted: !task?.isCompleted
+          }
+        }
+        else return task
+      })
+    })
+  }
+
   return (
     <div className={`${!props.open && "hidden"} h-screen bg-[#FAF9F8] flex flex-col`}>
       <div className='my-4 mx-6 py-2.5'>
@@ -100,15 +133,13 @@ function MyDay(props) {
               ...task,
               date: e.target.value
             })} />
-            {/* <svg class="fluentIcon dateButton-icon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 11a1 1 0 100-2 1 1 0 000 2zm1 2a1 1 0 11-2 0 1 1 0 012 0zm2-2a1 1 0 100-2 1 1 0 000 2zm1 2a1 1 0 11-2 0 1 1 0 012 0zm2-2a1 1 0 100-2 1 1 0 000 2zm4-5.5A2.5 2.5 0 0014.5 3h-9A2.5 2.5 0 003 5.5v9A2.5 2.5 0 005.5 17h9a2.5 2.5 0 002.5-2.5v-9zM4 7h12v7.5c0 .83-.67 1.5-1.5 1.5h-9A1.5 1.5 0 014 14.5V7zm1.5-3h9c.83 0 1.5.67 1.5 1.5V6H4v-.5C4 4.67 4.67 4 5.5 4z" fill="currentColor"></path></svg>
-            </button> */}
             <button className='p-1 mx-1.5'>
               <svg class="fluentIcon reminderButton-icon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a5.92 5.92 0 015.98 5.36l.02.22V11.4l.92 2.22a1 1 0 01.06.17l.01.08.01.13a1 1 0 01-.75.97l-.11.02L16 15h-3.5v.17a2.5 2.5 0 01-5 0V15H4a1 1 0 01-.26-.03l-.13-.04a1 1 0 01-.6-1.05l.02-.13.05-.13L4 11.4V7.57A5.9 5.9 0 0110 2zm1.5 13h-3v.15a1.5 1.5 0 001.36 1.34l.14.01c.78 0 1.42-.6 1.5-1.36V15zM10 3a4.9 4.9 0 00-4.98 4.38L5 7.6V11.5l-.04.2L4 14h12l-.96-2.3-.04-.2V7.61A4.9 4.9 0 0010 3z" fill="currentColor"></path></svg>
             </button>
             <button className='p-1 mx-1.5'>
               <svg class="fluentIcon recurringButton-icon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M16.5 6.67a.5.5 0 01.3.1l.08.07.01.02A5 5 0 0113.22 15L13 15H6.7l1.65 1.65c.18.17.2.44.06.63l-.06.07a.5.5 0 01-.63.06l-.07-.06-2.5-2.5a.5.5 0 01-.06-.63l.06-.07 2.5-2.5a.5.5 0 01.76.63l-.06.07L6.72 14h.14L7 14h6a4 4 0 003.11-6.52.5.5 0 01.39-.81zm-4.85-4.02a.5.5 0 01.63-.06l.07.06 2.5 2.5.06.07a.5.5 0 010 .56l-.06.07-2.5 2.5-.07.06a.5.5 0 01-.56 0l-.07-.06-.06-.07a.5.5 0 010-.56l.06-.07L13.28 6h-.14L13 6H7a4 4 0 00-3.1 6.52c.06.09.1.2.1.31a.5.5 0 01-.9.3A4.99 4.99 0 016.77 5h6.52l-1.65-1.65-.06-.07a.5.5 0 01.06-.63z" fill="currentColor"></path></svg>
             </button>
-            <button onClick={handleAddTask} disabled={task?.title.length===0}  className={`ml-auto px-2 ${task?.title.length>0?"text-blue-600 cursor-pointer":"text-gray-400 cursor-not-allowed"} bg-[#fbf9f6] border border-gray-200`}>Add</button>
+            <button onClick={handleAddTask} disabled={task?.title.length === 0} className={`ml-auto px-2 ${task?.title.length > 0 ? "text-blue-600 cursor-pointer" : "text-gray-400 cursor-not-allowed"} bg-[#fbf9f6] border border-gray-200`}>Add</button>
           </div>
         </div>
 
@@ -119,22 +150,48 @@ function MyDay(props) {
             <div className='py-2.5 px-4 w-28'>Due Date</div>
             <div className='py-2.5 px-4'>Importance</div>
           </div>
-
-          {allTasks.map((task,index)=>{
-            return(
- <div className='flex items-center bg-[#FAF9F8] rounded-b-lg border border-transparent border-t-[#d2cec9]'>
-            <div className='px-1.5' >
-              <svg class="fluentIcon checkBox-hover ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a8 8 0 110 16 8 8 0 010-16zm0 1a7 7 0 100 14 7 7 0 000-14zm3.36 4.65c.17.17.2.44.06.63l-.06.07-4 4a.5.5 0 01-.64.07l-.07-.06-2-2a.5.5 0 01.63-.77l.07.06L9 11.3l3.65-3.65c.2-.2.51-.2.7 0z" fill="currentColor"></path></svg>
-            </div>
-            <div className='py-2.5 px-4 w-150'>{task?.title}</div>
-            <div className='py-2.5 px-4 w-32'>{task?.date}</div>
-            <div className='my-3 mx-9.5'>
-              <Star onClick={()=>handleMarkImportant(index)} size={16} color={task?.isImportant?"yellow":"black"}/>
-            </div>
-          </div>
+          {allTasks.map((task, index) => {
+            if(task?.isCompleted){
+              return;
+            }
+            return (
+              <div key={index} className='flex items-center bg-[#FAF9F8] rounded-b-lg border border-transparent border-t-[#d2cec9]'>
+                <div className='px-1.5' >
+                  <button onClick={() => {
+                    handleMarkComplete(index)
+                    // handleCompleteTask
+                  }} color = {task?.isCompleted? "blue" : "black"}>
+                    <Circle  />
+                  </button>
+                  
+                </div>
+                <div className='py-2.5 px-4 w-150'> {task?.title} </div>
+                <div className='py-2.5 px-4 w-32'>{task?.date} </div>
+                <div className='my-3 mx-9.5'>
+                  <Star onClick={() => handleMarkImportant(index)} size={16} color={task?.isImportant ? "yellow" : "black"} />
+                </div>
+              </div>
             )
           })}
-         
+        </div>
+
+        <div className='flex flex-col justify-center shadow-md rounded-lg'>
+          <div>Completed</div>
+          {allTasks.map((task,index) => {
+            if(!task?.isCompleted){
+              return;
+            }
+            return (
+              <div key = {index} className='flex bg-[#FAF9F8] rounded-b-lg border border-transparent border-t-[#d2cec9]'>
+                <Circle onClick={() => handleMarkComplete(index)} color = {task?.isCompleted ? "blue" : "black"} background-color={task?.isCompleted ? "blue":"black"}/>
+                <div className=''>{task?.title}</div>
+                <div className='my-3 mx-9.5'>
+                  <Star  onClick={() => handleMarkImportant(index)} size={16} color={task?.isImportant ? "yellow" : "black"} />
+                </div>
+              </div>
+              
+            )
+          })}
         </div>
 
       </div>
