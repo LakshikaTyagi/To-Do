16450f1c212e9b94d037174d64a5bde5249e4424
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-06-10 22:53:07 +0530

    making code modular

--- src/pages/MyDay/MyDay.jsx
+++ src/pages/MyDay/MyDay.jsx
@@ -1,69 +1,13 @@
 import { useState } from 'react';
-import { getDate } from '../../../utils/utils';
 import { Star } from 'lucide-react';
 import { Circle } from 'lucide-react';
+import { getDate } from '../../../utils/utils';
+import Panel from '../Panel/Panel';
+
 
 function MyDay(props) {
-  const todayDate = getDate();
-  const [task, setTask] = useState({
-    id: "",
-    title: "",
-    date: "",
-    isImportant: false,
-    isCompleted : false,
-  });
-  const [allTasks, setAllTasks] = useState([]);
+      const todayDate = getDate();
 
-
-  const handleTaskInput = (e) => {
-    setTask({
-      ...task,
-      title: e.target.value
-    })
-  }
-
-  const handleAddTask = () => {
-    setAllTasks([
-      ...allTasks,
-      task
-    ])
-
-    setTask({
-      title: "",
-      date: "",
-      isImportant: false,
-      isCompleted : false,
-      id: "",
-    })
-  }
-
-  const handleMarkImportant = (index) => {
-    setAllTasks((tasks) => {
-      return tasks.map((task, i) => {
-        if (i === index) {
-          return {
-            ...task,
-            isImportant: !task?.isImportant
-          }
-        }
-        else return task
-      })
-    })
-  }
-
-  const handleMarkComplete = (index) => {
-    setAllTasks((tasks) => {
-      return tasks.map((task, i) => {
-        if(i === index){
-          return {
-            ...task,
-            isCompleted: !task?.isCompleted
-          }
-        }
-        else return task
-      })
-    })
-  }
 
   return (
     <div className={`${!props.open && "hidden"} h-screen bg-[#FAF9F8] flex flex-col`}>
@@ -106,79 +50,7 @@ function MyDay(props) {
         </div>
       </div>
 
-      <div className='mt-0 mb-0 grow flex flex-col gap-4 px-5'>
-
-        <div className='flex flex-col justify-center shadow-md rounded-lg'>
-          <div className='px-4 flex bg-[#FFFFFF] items-center rounded-t-lg'>
-            <svg className='ml-2.5 my-auto' class="fluentIcon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 3a7 7 0 100 14 7 7 0 000-14zm-8 7a8 8 0 1116 0 8 8 0 01-16 0z" fill="currentColor"></path></svg>
-            <input type="text" placeholder='Add a task' value={task?.title} onChange={handleTaskInput} name="" id="" className='outline-none w-full py-4 px-3.5' />
-          </div>
-          <div className='rounded-b-lg px-4 bg-[#FAF9F8] flex py-2 border border-transparent border-t-[#d2cec9]'>
-            <input className='p-1 mx-1.5' type='date' value={task?.date} onChange={(e) => setTask({
-              ...task,
-              date: e.target.value
-            })} />
-            <button className='p-1 mx-1.5'>
-              <svg class="fluentIcon reminderButton-icon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a5.92 5.92 0 015.98 5.36l.02.22V11.4l.92 2.22a1 1 0 01.06.17l.01.08.01.13a1 1 0 01-.75.97l-.11.02L16 15h-3.5v.17a2.5 2.5 0 01-5 0V15H4a1 1 0 01-.26-.03l-.13-.04a1 1 0 01-.6-1.05l.02-.13.05-.13L4 11.4V7.57A5.9 5.9 0 0110 2zm1.5 13h-3v.15a1.5 1.5 0 001.36 1.34l.14.01c.78 0 1.42-.6 1.5-1.36V15zM10 3a4.9 4.9 0 00-4.98 4.38L5 7.6V11.5l-.04.2L4 14h12l-.96-2.3-.04-.2V7.61A4.9 4.9 0 0010 3z" fill="currentColor"></path></svg>
-            </button>
-            <button className='p-1 mx-1.5'>
-              <svg class="fluentIcon recurringButton-icon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M16.5 6.67a.5.5 0 01.3.1l.08.07.01.02A5 5 0 0113.22 15L13 15H6.7l1.65 1.65c.18.17.2.44.06.63l-.06.07a.5.5 0 01-.63.06l-.07-.06-2.5-2.5a.5.5 0 01-.06-.63l.06-.07 2.5-2.5a.5.5 0 01.76.63l-.06.07L6.72 14h.14L7 14h6a4 4 0 003.11-6.52.5.5 0 01.39-.81zm-4.85-4.02a.5.5 0 01.63-.06l.07.06 2.5 2.5.06.07a.5.5 0 010 .56l-.06.07-2.5 2.5-.07.06a.5.5 0 01-.56 0l-.07-.06-.06-.07a.5.5 0 010-.56l.06-.07L13.28 6h-.14L13 6H7a4 4 0 00-3.1 6.52c.06.09.1.2.1.31a.5.5 0 01-.9.3A4.99 4.99 0 016.77 5h6.52l-1.65-1.65-.06-.07a.5.5 0 01.06-.63z" fill="currentColor"></path></svg>
-            </button>
-            <button onClick={handleAddTask} disabled={task?.title.length === 0} className={`ml-auto px-2 ${task?.title.length > 0 ? "text-blue-600 cursor-pointer" : "text-gray-400 cursor-not-allowed"} bg-[#fbf9f6] border border-gray-200`}>Add</button>
-          </div>
-        </div>
-
-        <div className='flex flex-col justify-center shadow-md rounded-lg'>
-          <div className='flex bg-[#FFFFFF] rounded-t-lg'>
-            <div className='py-2.5 px-4'> </div>
-            <div className='py-2.5 px-4 w-150'>Title</div>
-            <div className='py-2.5 px-4 w-28'>Due Date</div>
-            <div className='py-2.5 px-4'>Importance</div>
-          </div>
-          {allTasks.map((task, index) => {
-            if(task?.isCompleted){
-              return;
-            }
-            return (
-              <div key={index} className='flex items-center bg-[#FAF9F8] rounded-b-lg border border-transparent border-t-[#d2cec9]'>
-                <div className='px-1.5' >
-                  <button onClick={() => 
-                    handleMarkComplete(index)
-                  } color = {task?.isCompleted? "blue" : "black"}>
-                    <Circle  />
-                  </button>
-                  
-                </div>
-                <div className='py-2.5 px-4 w-150'> {task?.title} </div>
-                <div className='py-2.5 px-4 w-32'>{task?.date} </div>
-                <div className='my-3 mx-9.5'>
-                  <Star onClick={() => handleMarkImportant(index)} size={16} color={task?.isImportant ? "yellow" : "black"} />
-                </div>
-              </div>
-            )
-          })}
-        </div>
-
-        <div className='flex flex-col justify-center shadow-md rounded-lg'>
-          <div>Completed</div>
-          {allTasks.map((task,index) => {
-            if(!task?.isCompleted){
-              return;
-            }
-            return (
-              <div key = {index} className='flex bg-[#FAF9F8] rounded-b-lg border border-transparent border-t-[#d2cec9]'>
-                <Circle onClick={() => handleMarkComplete(index)} color = {task?.isCompleted ? "blue" : "black"} background-color={task?.isCompleted ? "blue":"black"}/>
-                <div className=''>{task?.title}</div>
-                <div className='my-3 mx-9.5'>
-                  <Star  onClick={() => handleMarkImportant(index)} size={16} color={task?.isImportant ? "yellow" : "black"} />
-                </div>
-              </div>
-              
-            )
-          })}
-        </div>
-
-      </div>
+      <Panel/>
 
     </div>
   )
--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -1,7 +1,14 @@
 import { ChevronDown } from 'lucide-react'
 import React from 'react'
+import { useState } from 'react';
+import { Star } from 'lucide-react';
+import { Circle } from 'lucide-react';
+import Panel from '../Panel/Panel';
 
 function Tasks(props) {
+  // const todayDate = getDate();
+  
+
   return (
     <div className={`${!props.open && "hidden"} h-screen flex flex-col bg-[#FAF9F8]`}>
       
@@ -29,48 +36,7 @@ function Tasks(props) {
         </div>
       </div>
 
-      <div className='mt-0 mb-0 grow flex flex-col gap-4 px-5'>
-
-        <div className='flex flex-col justify-center shadow-md rounded-lg'>
-          <div className='px-4 flex bg-[#FFFFFF] items-center rounded-t-lg'>
-            <svg className='ml-2.5 my-auto' class="fluentIcon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 3a7 7 0 100 14 7 7 0 000-14zm-8 7a8 8 0 1116 0 8 8 0 01-16 0z" fill="currentColor"></path></svg>
-            <input type="text" placeholder='Add a task' name="" id="" className='outline-none w-full py-4 px-3.5' />
-          </div>
-          <div className='rounded-b-lg px-4 bg-[#FAF9F8] flex py-2 border border-transparent border-t-[#d2cec9]'>
-            <button className='p-1 mx-1.5'>
-              <svg class="fluentIcon dateButton-icon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 11a1 1 0 100-2 1 1 0 000 2zm1 2a1 1 0 11-2 0 1 1 0 012 0zm2-2a1 1 0 100-2 1 1 0 000 2zm1 2a1 1 0 11-2 0 1 1 0 012 0zm2-2a1 1 0 100-2 1 1 0 000 2zm4-5.5A2.5 2.5 0 0014.5 3h-9A2.5 2.5 0 003 5.5v9A2.5 2.5 0 005.5 17h9a2.5 2.5 0 002.5-2.5v-9zM4 7h12v7.5c0 .83-.67 1.5-1.5 1.5h-9A1.5 1.5 0 014 14.5V7zm1.5-3h9c.83 0 1.5.67 1.5 1.5V6H4v-.5C4 4.67 4.67 4 5.5 4z" fill="currentColor"></path></svg>
-            </button>
-            <button className='p-1 mx-1.5'>
-              <svg class="fluentIcon reminderButton-icon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a5.92 5.92 0 015.98 5.36l.02.22V11.4l.92 2.22a1 1 0 01.06.17l.01.08.01.13a1 1 0 01-.75.97l-.11.02L16 15h-3.5v.17a2.5 2.5 0 01-5 0V15H4a1 1 0 01-.26-.03l-.13-.04a1 1 0 01-.6-1.05l.02-.13.05-.13L4 11.4V7.57A5.9 5.9 0 0110 2zm1.5 13h-3v.15a1.5 1.5 0 001.36 1.34l.14.01c.78 0 1.42-.6 1.5-1.36V15zM10 3a4.9 4.9 0 00-4.98 4.38L5 7.6V11.5l-.04.2L4 14h12l-.96-2.3-.04-.2V7.61A4.9 4.9 0 0010 3z" fill="currentColor"></path></svg>
-            </button>
-            <button className='p-1 mx-1.5'>
-              <svg class="fluentIcon recurringButton-icon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M16.5 6.67a.5.5 0 01.3.1l.08.07.01.02A5 5 0 0113.22 15L13 15H6.7l1.65 1.65c.18.17.2.44.06.63l-.06.07a.5.5 0 01-.63.06l-.07-.06-2.5-2.5a.5.5 0 01-.06-.63l.06-.07 2.5-2.5a.5.5 0 01.76.63l-.06.07L6.72 14h.14L7 14h6a4 4 0 003.11-6.52.5.5 0 01.39-.81zm-4.85-4.02a.5.5 0 01.63-.06l.07.06 2.5 2.5.06.07a.5.5 0 010 .56l-.06.07-2.5 2.5-.07.06a.5.5 0 01-.56 0l-.07-.06-.06-.07a.5.5 0 010-.56l.06-.07L13.28 6h-.14L13 6H7a4 4 0 00-3.1 6.52c.06.09.1.2.1.31a.5.5 0 01-.9.3A4.99 4.99 0 016.77 5h6.52l-1.65-1.65-.06-.07a.5.5 0 01.06-.63z" fill="currentColor"></path></svg>
-            </button>
-            <button className='ml-auto px-2 text-gray-400 bg-[#fbf9f6] border border-gray-200'>Add</button>
-          </div>
-        </div>
-
-        <div className='flex flex-col justify-center shadow-md rounded-lg'>
-          <div className='flex bg-[#FFFFFF] rounded-t-lg'>
-            <div className='py-2.5 px-4'> </div>
-            <div className='py-2.5 px-4 w-150'>Title</div>
-            <div className='py-2.5 px-4 w-28'>Due Date</div>
-            <div className='py-2.5 px-4'>Importance</div>
-          </div>
-          <div className='flex items-center bg-[#FAF9F8] rounded-b-lg border border-transparent border-t-[#d2cec9]'>
-            <div className='px-1.5' >
-              <svg class="fluentIcon checkBox-hover ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a8 8 0 110 16 8 8 0 010-16zm0 1a7 7 0 100 14 7 7 0 000-14zm3.36 4.65c.17.17.2.44.06.63l-.06.07-4 4a.5.5 0 01-.64.07l-.07-.06-2-2a.5.5 0 01.63-.77l.07.06L9 11.3l3.65-3.65c.2-.2.51-.2.7 0z" fill="currentColor"></path></svg>
-            </div>
-            <div className='py-2.5 px-4 w-150'>Learn</div>
-            <div className='py-2.5 px-4 w-28'></div>
-            <div className='my-3 mx-9.5'>
-              <svg class="fluentIcon ___12fm75w f1w7gpdv fez10in fg4l7m0" aria-label="" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.1 2.9a1 1 0 011.8 0l1.93 3.91 4.31.63a1 1 0 01.56 1.7l-3.12 3.05.73 4.3a1 1 0 01-1.45 1.05L10 15.51l-3.86 2.03a1 1 0 01-1.45-1.05l.74-4.3L2.3 9.14a1 1 0 01.56-1.7l4.31-.63L9.1 2.9zm.9.44L8.07 7.25a1 1 0 01-.75.55L3 8.43l3.12 3.04a1 1 0 01.3.89l-.75 4.3 3.87-2.03a1 1 0 01.93 0l3.86 2.03-.74-4.3a1 1 0 01.29-.89L17 8.43l-4.32-.63a1 1 0 01-.75-.55L10 3.35z" fill="currentColor"></path></svg>
-            </div>
-
-          </div>
-        </div>
-
-      </div>
+      <Panel/>
 
     </div>
   )
